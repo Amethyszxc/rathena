72a30a28448f0a4d25d07ac56a7b2848aaa25556
Amethyszxc <109427608+Amethyszxc@users.noreply.github.com>
2022-11-12 07:25:47 +0800

    Revert "Itemlink command and integration (#7291)"
    This reverts commit 55d3c1578cc6e9cc925d17f23fbacc8a0bafd8d0.

--- src/map/itemdb.hpp
+++ src/map/itemdb.hpp
@@ -5,7 +5,6 @@
 #define ITEMDB_HPP
 
 #include <map>
-#include <string>
 #include <vector>
 
 #include "../common/database.hpp"
@@ -1345,8 +1344,6 @@ public:
 	// Additional
 	std::shared_ptr<item_data> searchname( const char* name );
 	std::shared_ptr<item_data> search_aegisname( const char *name );
-	std::string create_item_link( struct item& data );
-	std::string create_item_link( t_itemid id );
 };
 
 extern ItemDatabase item_db;
